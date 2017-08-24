a0f6fcc688cc81ea129387b6d8d2322d68d51590
periyasamy <peri@gettalent.in>
2017-08-24 15:32:34 +0530

    Changes in components

--- src/app/restaurant.service.ts
+++ src/app/restaurant.service.ts
@@ -14,14 +14,8 @@ export class RestaurantService {
 restaurantPlaceApi(lat,lan){
 
 console.log(lan,lat,"asdads")
-		// // var headers = new Headers();
 		let headers = new Headers();
-		// headers.append('Access-Control-Allow-Origin', '*')
 		headers.append('Content-Type', 'application/x-www-form-urlencoded');
-		// headers.append("Access-Control-Allow-Credentials", "true");
-		// headers.append('Access-Control-Allow-Origin', '*');
-		// headers.append('Access-Control-Allow-Methods','POST,GET,PUT,DELETE');
-		// headers.append('Access-Control-Allow-Headers','Authorization, Lang');
 		return this.http.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+lat+','+lan+'&radius=500&type=restaurant&keyword=cruise&key=AIzaSyBS5VM_9o7FKZYe2kzmxQ_s5sxxtCzCm8c',{headers:headers})
 			.map(res => res.json())
 			.catch(this.handleError);
